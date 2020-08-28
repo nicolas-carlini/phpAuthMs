29b472e50608363c5520df0eac6955f169fd33ce
NicolasCarlini <nicolas.carlini@fizzmod.com>
2020-08-27 23:44:35 -0300

    docker

--- backend/web/public/api/validate.php
+++ backend/web/public/api/validate.php
@@ -1,8 +1,9 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
