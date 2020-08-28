38c387ce653097ab58faf1426fe836ad53825e63
NicolasCarlini <nicolas.carlini@fizzmod.com>
2020-08-28 00:19:38 -0300

    api update

--- backend/web/public/api/signin.php
+++ backend/web/public/api/signin.php
@@ -15,18 +15,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   try {
     if($pwd != null && $email != null){
       $result = $User->signin($email,$pwd);
-      $payload = ["isLogged" => $result[0], "error" => $result[1], "email"=>$email, "pwd"=>$pwd];
+      $payload = ["isLogged" => $result[0], "error" => $result[1]];
     }
     else{
-      $payload = ["isLogged" => false, "error" => true, "email"=>$email, "pwd"=>$pwd];
+      $payload = ["isLogged" => false, "error" => true];
     }
   } catch (Exception $e) {
     echo $e;
-    $payload = ["isLogged" => false, "error" => true, "email"=>$email, "pwd"=>$pwd];
+    $payload = ["isLogged" => false, "error" => true];
   } finally {
     echo json_encode($payload);
   }
 } else {
-  $payload = ["isLogged" => false, "error" => true, "postman"=> true];
+  $payload = ["isLogged" => false, "error" => true];
   echo json_encode($payload);
 }
